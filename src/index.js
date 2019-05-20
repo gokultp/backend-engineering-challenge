67d19f141f33ea552b228a190c8d7a60aaec7e54
Gokul T P <tp.gokul@gmail.com>
2019-05-20 20:06:09 +0530

    updated argument parser

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import Aggregator from './aggregator'
 const fs = require('fs');
-const es  = require('event-stream');
+const commandLineArgs = require('command-line-args')
+
+const defCommands = ()=>([
+    {name: 'window_size', alias: 'w', type: Number},
+    {name: 'input_file', alias: 'i', type: String}
+]);
+
 
 
 const main =()=>{
-    let input = fs.createReadStream("/tmp/test");
+    let args = commandLineArgs(defCommands());
+    if (!args.input_file) {
+        console.log("No filename provided");
+        process.exit(1);
+    }
+    if (!args.window_size) {
+        console.log("No window size provided");
+        process.exit(1);
+    }
+    let input = fs.createReadStream(args.input_file);
     let output = process.stdout;
-    let agg = new Aggregator(input, output, 10);
-    agg.exec();
+    let agg = new Aggregator(input, output, args.window_size);
+    try {
+        agg.exec();
+    }catch(err){
+        console.log(err)
+        process.exit(2);
+    }
 }
 
 main();
